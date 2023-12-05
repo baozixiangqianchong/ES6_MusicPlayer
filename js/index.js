e75c5d8153353434c02049fe9dbebe045830ece1
yourname <1741349700@qq.com>
2023-12-05 09:31:34 +0800

    add: 响应式数据

--- js/index.js
+++ js/index.js
@@ -1,22 +1,10 @@
-window.addEventListener("load", () => {
-    // const script = document.createElement("SCRIPT");
-    // const script1 = document.createElement("SCRIPT");
-
-    // script.src = "./js/home/home.js";
-    // script1.src = "./js/home/carousel.js";
-
-    // script.setAttribute("type", "module");
-    // document.querySelector("body").appendChild(script)
-    // script1.setAttribute("type", "module");
-    // document.querySelector("body").appendChild(script1)
-
-    hash = window.location.hash;
-    changeComponent();
-})
-
 //导入对应的页面初始化函数
 import { homePage } from "./home/home.js";
 import { recommendListPage } from "./recommendList/recommendList.js";
+import { reactive } from "./util/reactive.js";
+//导入hash信息提取函数
+import { getRouteroptions } from "./util/util.js";
+
 //路由表
 const routers = [
     {
@@ -31,20 +19,47 @@ const routers = [
     }
 ];
 
-//导入hash信息提取函数
-import { getRouteroptions } from "./util/util.js";
-let hash;
+// let hash;
+
 function changeComponent() {
-    let options = getRouteroptions(hash);
-    const [{ component }] = routers.filter(
+    let options = getRouteroptions(hashProxy.hash);
+    const [{ component, name }] = routers.filter(
         (router) => router.name == options.name
     );
 
     component(options);
-    document.querySelector("#header-title").innerHTML = options.name;
+    document.querySelector("#header-title").innerHTML = name;
 }
 
 window.addEventListener("hashchange", () => {
-    hash = window.location.hash;
-    changeComponent();
+    // hash = window.location.hash;
+    // changeComponent();
+    hashProxy.hash = window.location.hash;
 });
+// 监听页面 load和hashchange 事件，事件触发后对代理对象赋值
+window.addEventListener("load", () => {
+    // const script = document.createElement("SCRIPT");
+    // const script1 = document.createElement("SCRIPT");
+
+    // script.src = "./js/home/home.js";
+    // script1.src = "./js/home/carousel.js";
+
+    // script.setAttribute("type", "module");
+    // document.querySelector("body").appendChild(script)
+    // script1.setAttribute("type", "module");
+    // document.querySelector("body").appendChild(script1)
+
+    // hash = window.location.hash;
+    // changeComponent();
+    hashProxy.hash = window.location.hash;
+})
+
+//数据响应式执行函数
+let effective = () => changeComponent();
+// 数据响应式处理
+export const hashProxy = reactive(
+    {
+        hash: ""
+    },
+    effective
+)
